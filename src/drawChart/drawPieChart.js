5e0e5022abd2837e3c05f8f0116916d52cbfe781
igor.boiko <igor.boiko@socialtechnologies.io>
2024-09-29 10:39:46 +0300

    feat: Refactor pie chart

--- src/drawChart/drawPieChart.js
+++ src/drawChart/drawPieChart.js
@@ -29,8 +29,7 @@ export default function drawPieChart(data, title, colors) {
     const totalSteps = 200;
     const step = 1 / totalSteps;
 
-    const drawLegend = () => {
-        // Draw legend
+    function drawLegend() {
         const legendX = width / 2 + radius + padding / 2;
         const legendY = padding;
         const legendBoxSize = 20;
@@ -39,25 +38,28 @@ export default function drawPieChart(data, title, colors) {
         data.slice(1).forEach((value, index) => {
             const legendItemY = legendY + index * (legendBoxSize + legendSpacing);
 
-            // Draw legend color box
             ctx.fillStyle = colors[index % colors.length];
             ctx.fillRect(legendX, legendItemY, legendBoxSize, legendBoxSize);
 
-            // Draw legend text
             ctx.fillStyle = textColor;
             ctx.font = '16px Arial';
             ctx.textAlign = 'left';
             ctx.textBaseline = 'middle';
             ctx.fillText(value[0], legendX + legendBoxSize + legendSpacing, legendItemY + legendBoxSize / 2);
         });
-    };
+    }
 
-    const animate = () => {
-        progress += step;
-        if (progress > 1) progress = 1;
+    function drawTitle() {
+        if (title) {
+            ctx.fillStyle = textColor;
+            ctx.font = '24px Arial';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'top';
+            ctx.fillText(title, 0, -height / 2 + basePadding / 2);
+        }
+    }
 
-        ctx.clearRect(-width / 2, -height / 2 - titlePadding / 2, width / 2 + radius + padding / 2, height);
-
+    function drawPieSlices() {
         let startAngle = 0;
         let endAngle = 0;
         angles.forEach((angle, index) => {
@@ -73,46 +75,47 @@ export default function drawPieChart(data, title, colors) {
             ctx.fill();
             startAngle = endAngle;
         });
+    }
 
-        // Draw title
-        if (title) {
+    function drawPercentages() {
+        let startAngle = 0;
+        let endAngle = 0;
+        angles.forEach((angle, index) => {
+            endAngle += angle;
+            const textAngle = startAngle + (endAngle - startAngle) / 2;
+            const textX = (radius * 0.75) * Math.cos(textAngle);
+            const textY = (radius * 0.75) * Math.sin(textAngle);
+            const percentage = ((Number(data[index + 1][1]) / total) * 100).toFixed(2);
+
             ctx.fillStyle = textColor;
-            ctx.font = '24px Arial';
+            ctx.font = '16px Arial';
             ctx.textAlign = 'center';
-            ctx.textBaseline = 'top';
-            ctx.fillText(title, 0, -height / 2 + basePadding / 2);
-        }
+            ctx.textBaseline = 'middle';
+            ctx.fillText(`${percentage}%`, textX, textY);
+
+            startAngle = endAngle;
+        });
+    }
+
+    function animate() {
+        progress += step;
+        if (progress > 1) progress = 1;
+
+        ctx.clearRect(-width / 2, -height / 2 - titlePadding / 2, width / 2 + radius + padding / 2, height);
+
+        drawPieSlices();
+        drawTitle();
 
         if (progress < 1) {
             requestAnimationFrame(animate);
         } else {
-            // Draw percentages
-            startAngle = 0;
-            endAngle = 0;
-            angles.forEach((angle, index) => {
-                endAngle += angle;
-                const textAngle = startAngle + (endAngle - startAngle) / 2;
-                const textX = (radius * 0.75) * Math.cos(textAngle);
-                const textY = (radius * 0.75) * Math.sin(textAngle);
-                const percentage = ((Number(data[index + 1][1]) / total) * 100).toFixed(2);
-
-                // Draw the percentage text
-                ctx.fillStyle = textColor;
-                ctx.font = '16px Arial';
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-                ctx.fillText(`${percentage}%`, textX, textY);
-
-                startAngle = endAngle;
-            });
-
+            drawPercentages();
             ctx.restore();
-            // Draw legend after animation completes
             drawLegend();
         }
-    };
+    }
 
-    const showTooltip = (event) => {
+    function showTooltip(event) {
         const rect = canvas.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
@@ -143,7 +146,7 @@ export default function drawPieChart(data, title, colors) {
             }
         }
         tooltip.style.display = 'none';
-    };
+    }
 
     canvas.addEventListener('mousemove', showTooltip);
     canvas.addEventListener('mouseout', () => {
